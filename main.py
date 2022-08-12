3dd173af940e6eab98666b3354624b039193b188
Chandu S <93590934+FlareWings@users.noreply.github.com>
2022-08-12 14:37:21 +0530

    Bug fix

--- main.py
+++ main.py
@@ -14,9 +14,9 @@ import mlflow.sklearn
 
 def eval_metrics(actual, pred):
     Accuracy_Score = round(accuracy_score(actual, pred), 2)
-    Precision_score = round(precision_score(y_test, Predictions, average='micro'),2)
-    Recall_score = round(recall_score(y_test, Predictions, average='micro'),2)
-    F1_score = round(f1_score(y_test, Predictions, average='micro'),2)
+    Precision_score = round(precision_score(y_test, pred, average='micro'),2)
+    Recall_score = round(recall_score(y_test, pred, average='micro'),2)
+    F1_score = round(f1_score(y_test, pred, average='micro'),2)
     return Accuracy_Score, Precision_score, Recall_score, F1_score
 
 
